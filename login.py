9ff1f08e818a8d9ec4c26eaa7d5315a437aebe68
nlessone <nlessone@gmail.com>
2013-07-05 20:51:54 +0600

    Add global variables for db connection

--- login.py
+++ login.py
@@ -1,5 +1,6 @@
 import cgi
 import pymysql
+import os
 
 print("Content-Type: text/html")
 print("")
@@ -10,8 +11,15 @@ form = cgi.FieldStorage()
 login = form.getvalue('login')
 password  = form.getvalue('password')
 
+#Получаем переменные окружения
+host = os.environ['OPENSHIFT_MYSQL_DB_HOST']
+port = os.environ['OPENSHIFT_MYSQL_DB_PORT']
+user = os.environ['OPENSHIFT_MYSQL_DB_USERNAME']
+passwd = os.environ['OPENSHIFT_MYSQL_DB_PASSWORD']
+db = os.environ['OPENSHIFT_APP_NAME']
+
 #Работа с базой
-conn = pymysql.connect(host='127.0.0.1', port=3306, user='root', passwd='pen3souin', db='paintinggamedb')
+conn = pymysql.connect(host, port, user, passwd, db)
 cur = conn.cursor()
 #TODO сделать отлов ошибок MySQL, например на уникальность логина/мыла и т.п
 cur.execute("SELECT id FROM pgusers where login=%s and password=%s", (login, password))
--- registration.py
+++ registration.py
@@ -1,5 +1,6 @@
 import cgi
 import pymysql
+import os
 
 #print("Content-Type: text/html")
 # Create instance of FieldStorage
@@ -10,8 +11,15 @@ login = form.getvalue('login')
 password  = form.getvalue('password')
 email = form.getvalue('email')
 
+#Получаем переменные окружения
+host = os.environ['OPENSHIFT_MYSQL_DB_HOST']
+port = os.environ['OPENSHIFT_MYSQL_DB_PORT']
+user = os.environ['OPENSHIFT_MYSQL_DB_USERNAME']
+passwd = os.environ['OPENSHIFT_MYSQL_DB_PASSWORD']
+db = os.environ['OPENSHIFT_APP_NAME']
+
 #Работа с базой
-conn = pymysql.connect(host='127.0.0.1', port=3306, user='root', passwd='pen3souin', db='paintinggamedb')
+conn = pymysql.connect(host, port, user, passwd, db)
 cur = conn.cursor()
 #TODO сделать отлов ошибок MySQL, например на уникальность логина/мыла и т.п
 cur.execute("INSERT INTO pgusers(Login,Password,Mail) VALUES (%s,%s,%s)", (login, password, email))
